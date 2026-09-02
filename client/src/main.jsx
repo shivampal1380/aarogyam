d00edac34686dd528cffab8b46f7c707319d18c4
shivam_pal <pal.shivam1380@gmail.com>
2026-09-02 22:43:55 +0530

    Fix routing on GitHub Pages by using HashRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "react-hot-toast";
 import App from "./App";
@@ -9,7 +9,7 @@ import "./styles/index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Toaster
           position="top-right"
           toastOptions={{
@@ -30,7 +30,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           }}
         />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </React.StrictMode>
 );
